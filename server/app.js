29be75cfc3f6a9815e86c69e556ad03ef1b2cb29
viincyy <viincyy126@gmail.com>
2021-08-31 18:58:45 +0700

    Create Apollo server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+// LOAD SCHEMA/ RESOLVER
+const typeDefs = require('./schema/schema');
+const resolvers = require('./resolver/resolver');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+
+server.applyMiddleware({
+    app,
+});
+
+app.listen({ PORT: 4000 }, () =>
+    console.log('Server is starting at http://localhost:4000')
+);
