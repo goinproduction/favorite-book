faf7c3abee5f27253b3ca486be5d4a3f49b0bb16
viincyy <viincyy126@gmail.com>
2021-08-31 18:32:20 +0700

    Graph Schema

---
+++ server/schema/schema.js
@@ -0,0 +1,16 @@
+const { qgl } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book {
+        id: ID
+        name: String
+        genre: String
+    }
+
+    # ROOT TYPE
+    type Query {
+        books: [Book]
+    }
+`;
+
+module.exports = typeDefs;
